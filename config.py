68ef966e76db152f832b6513ba8fc7423a38de3a
Shariq Ansari <shariqan@oit.rutgers.edu>
2015-02-17 13:57:19 -0500

    Add files missing from last commit.

---
+++ config.py
@@ -0,0 +1,9 @@
+import yaml
+import pkg_resources
+
+def reload():
+    __config_file__ = pkg_resources.resource_string(__name__, "config.yml")
+    __config_dict__ = yaml.load(__config_file__)
+    globals().update(__config_dict__)
+
+reload() # Initialize the globals on the first load of this module
