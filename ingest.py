357d62a72361ea7b72e182cf48581cae3c338869
Shariq Ansari <shariqan@oit.rutgers.edu>
2015-02-18 16:00:57 -0500

    - Update configuration options. - Fix terminology as it relates to EDEX environment sourcing. - Begin framework for checking for already ingested files.

--- ingest.py
+++ ingest.py
@@ -14,18 +14,18 @@ import logging
 import subprocess
 import sys
 import time
-from config import SLEEP_TIMER, INGESTION, EDEX
+from config import SLEEP_TIMER, UFRAME, EDEX
 
 EDEX_LOG_FILES = glob.glob("%s%s" % (EDEX['log_path'], "edex-ooi*.log"))
 
 # Set up some basic logging.
 logging.basicConfig(level=logging.INFO)
 handler = logging.FileHandler(
-    INGESTION['log_path'] + '/' + 'ingestion.log'
+    UFRAME['log_path'] + '/' + 'ingestion.log'
     )
 handler.setLevel(logging.INFO)
 handler.setFormatter(
-    logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     )
 logger = logging.getLogger(__name__)
 logger.addHandler(handler)
@@ -38,10 +38,8 @@ class Ingest(object):
 
     @classmethod
     def _common_options(cls, args):
-        test_mode = False
+        test_mode = "-t" in args
         sleep_timer = SLEEP_TIMER
-        if "-t" in args:
-            test_mode = True
         try:
             sleep_timer = int([a for a in args if a[:8]=="--sleep="][0].split("=")[1])
         except IndexError:
@@ -52,29 +50,28 @@ class Ingest(object):
         return test_mode, sleep_timer
 
     @classmethod
-    def _start_edex(cls, fake=False):
-        ''' Starts the EDEX server. The keyword argument 'fake' '''
-        if fake:
-            logger.info("Starting the EDEX server.")
-            logger.info("EDEX server started.")
+    def _start_edex(cls, test_mode=False):
+        ''' Starts the EDEX server. The keyword argument 'test_mode' is used to bypass actually 
+            starting the EDEX server for testing purposes. '''
+
+        if test_mode:
+            logger.info("TEST MODE: Sourcing the EDEX server environment.")
+            logger.info("TEST MODE: EDEX server environment sourced.")
             return
 
-        # Start the EDEX server.
+        # Source the EDEX environment.
         try:
-            logger.info(
-                "Starting the EDEX server.")
+            logger.info("Sourcing the EDEX server environment.")
             run_edex = subprocess.check_output(['source', EDEX['command']])
-        except subprocess.CalledProcessError as e:
-            logger.error(
-                "There was a problem starting the EDEX server (Error code %s)" % e.returncode)
-            sys.exit(4)
         except Exception:
             logger.exception(
-                "A system error occurred when starting the EDEX server.")
+                "An error occurred when sourcing the EDEX server environment.")
             sys.exit(4)
         else:
-            logger.info(
-                "EDEX server started.")
+            logger.info("EDEX server environment sourced.")
+
+        # Insert code for starting the EDEX server here.
+
         return
 
     @classmethod
@@ -86,19 +83,24 @@ class Ingest(object):
             This function will return a list of ingestions that failed. A blank list indicates 
             success.'''
 
+        # Define some helper methods.
+        def track_failure(file, route, designator, source):
+            return {
+                'filename_mask': file, 
+                'uframe_route': route, 
+                'reference_designator': designator, 
+                'data_source': source,
+                }
+
+        def _already_ingested(datafile):
+            return False
+
         # Get a list of files that match the file mask and log the list size.
         data_files = glob.glob(filename_mask)
         logger.info("%s file(s) found for %s" % (len(data_files), filename_mask))
 
         # Keep a running list of failed ingestions.
         failed_ingestions = []
-        def track_failure(file, route, designator, source):
-            return {
-                'filename_mask': file,
-                'uframe_route': route,
-                'reference_designator': designator,
-                'data_source': source,
-                }
 
         # If no files are found, consider the entire filename mask a failure and track it.
         if len(data_files) == 0:
@@ -108,13 +110,17 @@ class Ingest(object):
 
         # Ingest each file in the file list.
         for data_file in data_files:
+            if _already_ingested(data_file):
+                logger.info("%s has already been ingested." % data_file)
+                continue
+
             ingestion_command = (
-                INGESTION['command'], uframe_route, data_file, reference_designator, data_source
-                )
+                UFRAME['command'], uframe_route, data_file, reference_designator, data_source)
             try:
                 # Attempt to send the file to UFrame's ingest sender.
+                message = " ".join(ingestion_command)
                 if test_mode:
-                    logger.info("TEST MODE: %s" % " ".join(ingestion_command))
+                    message = "TEST MODE: " + message
                 else:
                     subprocess.check_output(ingestion_command)
             except subprocess.CalledProcessError as e:
@@ -132,14 +138,7 @@ class Ingest(object):
                     track_failure(data_file, uframe_route, reference_designator, data_source))
             else:
                 # If the ingest sender returns without any error code, consider a success and log it.
-                if test_mode:
-                    logger.info(
-                        "TEST MODE: %s submitted to UFrame for ingestion (%s, %s, %s)." % (
-                            data_file, uframe_route, reference_designator, data_source))
-                else:
-                    logger.info(
-                        "%s submitted to UFrame for ingestion (%s, %s, %s)." % (
-                            data_file, uframe_route, reference_designator, data_source))
+                logger.info(message)
         time.sleep(sleep_timer)
         return failed_ingestions
 
@@ -163,13 +162,11 @@ class Ingest(object):
             sys.exit(3)
         fieldnames = ['uframe_route', 'filename_mask', 'reference_designator', 'data_source']
         if reader.fieldnames != fieldnames:
-            print reader.fieldnames == fieldnames
             logger.error("Mapping CSV does not have valid column headers.")
             sys.exit(3)
 
         # Start the EDEX server.
-        if not test_mode:
-            self.__class__._start_edex(fake=EDEX['fake'])
+        self.__class__._start_edex(test_mode=EDEX['test_mode'] or test_mode)
 
         # Run ingestions for each row in the CSV, keeping track of any failures.
         failed = []
@@ -183,14 +180,15 @@ class Ingest(object):
         # Write any failed ingestions out into a CSV file that can be re-ingested later.
         if failed:
             date_string = datetime.datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
-            outfile = INGESTION["failed_path"] + "/" + "failed_ingestions_" + date_string + '.csv'
+            outfile = UFRAME["failed_ingestion_path"] + "/" + "failed_ingestions_" + date_string + '.csv'
             logger.info(
                 "Writing %s failed ingestion(s) out to %s" % (len(failed), outfile))
             writer = csv.DictWriter(open(outfile, 'wb'), delimiter=',', fieldnames=fieldnames)
             writer.writerow(dict((fn,fn) for fn in fieldnames))
             for f in failed:
                 writer.writerow(f)
-        return
+        logger.info("Ingestion task from_csv for %s completed with %s failure(s)." % (csv_file, len(failed)))
+
 
 if __name__ == '__main__':
     perform = Ingest()
