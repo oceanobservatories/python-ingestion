be6e743ec221a567868c3d27982780e2cd15002a
Shariq Ansari <shariqan@oit.rutgers.edu>
2016-03-30 12:00:16 -0400

    Continued work on Web UI for Ingestion - update .gitignore files - add django-polymorphic to base requirements for www - add unique restrictions on deployment app models' fields - add logic and custom exception to handle when a user attempts to create a deployment that already exists - add Action polymorphic model to track events - add DeploymentAction model to track events related to Deployments - add url querystring filtering on deployments list page with links on deployment detail pages - add Actions table to deployment detail pages - add function to reprocess a deployment's CSV file from the deployment detail page - add request to context processors - make various adjustments to CSS and HTML
    This update adds django-polymorphic to the www side of the application. Ensure that the requirements file in www is reinstalled via pip.

--- www/src/deployments/views.py
+++ www/src/deployments/views.py
@@ -1,4 +1,5 @@
-from django.http import Http404
+from django.contrib import messages
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic 
 
@@ -7,6 +8,16 @@ from deployments.forms import DeploymentCreateFromCSVForm
 
 class DeploymentListView(generic.ListView):
     model = Deployment
+
+    def get_queryset(self):
+        filters = {
+            'platform__reference_designator': self.request.GET.get('platform', None), 
+            'number': self.request.GET.get('number', None), 
+            'data_source__name': self.request.GET.get('data_source', None), 
+            'data_source__abbr': self.request.GET.get('data_source_abbr', None), 
+            }
+        filters = {f: filters[f] for f in filters if filters[f]}
+        return Deployment.objects.filter(**filters)
 
 class DeploymentDetailView(generic.DetailView):
     model = Deployment
@@ -17,13 +28,32 @@ class DeploymentDetailView(generic.DetailView):
         except:
             raise Http404(u"No deployment found.")
 
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if "_process_csv" in request.POST:
+            self.object.process_csv()
+            self.object.log_action(request.user, 
+                "Processed CSV and created %d new data groups." % self.object.data_groups.count())
+        return HttpResponseRedirect(self.object.get_absolute_url())
+
 class DeploymentCreateView(generic.edit.FormView):
     template_name = "deployments/deployment_form.html"
     form_class = DeploymentCreateFromCSVForm
-    success_url = "/deployments/"
 
     def form_valid(self, form):
         csv_file = form.cleaned_data.get('csv_file', None)
-        new_deployment = Deployment.create_from_csv_file(csv_file)
-        new_deployment.process_csv()
+        try:
+            self.new_deployment = Deployment.create_from_csv_file(csv_file)
+            self.new_deployment.log_action(self.request.user, "Created.")
+        except Deployment.AlreadyExists, e:
+            msg = "%s %s." % (e.message, e.html_link_to_object("View the deployment"))
+            messages.warning(self.request, msg)
+            return super(DeploymentCreateView, self).form_invalid(form)
+        else:
+            self.new_deployment.process_csv()
+            self.new_deployment.log_action(self.request.user, 
+                "Processed CSV and created %d new data groups." % self.new_deployment.data_groups.count())
         return super(DeploymentCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return self.new_deployment.get_absolute_url()
