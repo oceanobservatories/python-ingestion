5ca4561996c22ca29bcad497ce3289a5b506c850
Shariq Ansari <shariqan@oit.rutgers.edu>
2016-04-07 15:05:12 -0400

    Add login required decorators to deployment views.

--- www/src/deployments/urls.py
+++ www/src/deployments/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required, permission_required
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.DeploymentListView.as_view(), name="list"),
-    url(r'^create/$', views.DeploymentCreateView.as_view(), name="create"),
-    url(r'^detail/(?P<slug>[-\w]+)/$', views.DeploymentDetailView.as_view(), name='detail'),
+    url(r'^$', login_required(views.DeploymentListView.as_view()), name="list"),
+    url(r'^create/$', login_required(views.DeploymentCreateView.as_view()), name="create"),
+    url(r'^detail/(?P<slug>[-\w]+)/$', login_required(views.DeploymentDetailView.as_view()), name='detail'),
     ]
